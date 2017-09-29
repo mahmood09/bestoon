b6a920066832d264290588f298e12c77670783a2
Mahmood <mda@Mahmoods-MacBook-Pro.local>
2017-09-29 17:33:51 +0330

    income in models and some beauti

--- web/models.py
+++ web/models.py
@@ -11,3 +11,16 @@ class Expense(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
 
+    def __unicode__(self):
+        return "{} | {} | {} ".format(self.text,self.date,self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return "{} | {} | {} ".format(self.text, self.date, self.amount)
+
+
